167cc374a8d6504d9511f01d6f992c09cfd87ad8
KhubaibGhouri <khubaibghouri@gmail.com>
2017-04-26 22:39:53 +0500

    first commit

---
+++ src/app/coming-soon/coming-soon.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-coming-soon',
+  templateUrl: './coming-soon.component.html',
+  styleUrls: ['./coming-soon.component.css']
+})
+export class ComingSoonComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+  imageUrl='./assets/images/left-side.png';
+  imageUrl2='./assets/images/right-side.png';
+  invelop='./assets/images/envelop.png';
+
+}
